db63df7609ed1be7462b2bde294f1381ba8d16b2
liutongchun <17681102655@163.com>
2019-11-22 20:49:42 +0800

    commit

---
+++ user-common/src/main/java/user/common/utils/Md5Utils.java
@@ -0,0 +1,14 @@
+package user.common.utils;
+
+/**
+ * @Auther: ltc
+ * @Date: 2019/11/21 13:55
+ * @Description:
+ */
+public class Md5Utils {
+
+    public static void main(String[] args) {
+        String s = "17681102655";
+
+    }
+}
